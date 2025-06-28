84b6727b9458ac24f60d9a0e2febd82a0b5c7085
TeeGarden100500 <hexus1984ab@gmail.com>
2025-06-28 21:15:05 +0400

    Add dummy HTTP server for Render

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
-const { launchBot } = require('./handlers/telegramHandler');
-const { watchTokens } = require('./services/tokenWatcher');
-
 console.log('Million Accelerator Bot started');
-launchBot();
-watchTokens();
+
+// ==== Render Port Binding Dummy Server ====
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Million Accelerator Bot is running.\n');
+}).listen(PORT, () => {
+  console.log(`Dummy server listening on port ${PORT}`);
+});
